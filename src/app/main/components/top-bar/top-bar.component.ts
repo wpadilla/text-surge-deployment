3823ffb5e9818f8101893026c4496bd3f7df8b13
jeff ritenburg <jeff@ns804.com>
2021-05-08 14:25:18 -0400

    added generic top bar to main views

---
+++ src/app/main/components/top-bar/top-bar.component.ts
@@ -0,0 +1,14 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'ts-top-bar',
+  templateUrl: './top-bar.component.html',
+  styleUrls: ['./top-bar.component.scss']
+})
+export class TopBarComponent implements OnInit {
+    constructor() { }
+
+    public ngOnInit() {
+
+    }
+}
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { MainComponent } from './containers/main/main.component';
 import { SharedModule } from '../shared/shared.module'
 import { MainRoutingModule } from './main-routing.module';
-// import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { SideNavComponent } from './components/side-nav/side-nav.component';
 
 
 
 @NgModule({
-  declarations: [MainComponent, /* TopBarComponent, */ SideNavComponent],
+  declarations: [MainComponent, TopBarComponent, SideNavComponent],
   imports: [
     SharedModule,
     MainRoutingModule
