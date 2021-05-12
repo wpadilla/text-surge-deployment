9dc341d96f743f1619ac72cda8918eae1997e06d
Williams <wilfre_padilla@hotmail.com>
2021-05-12 08:34:28 -0400

    Feat: order by functionality/

--- src/app/shared/components/list-filters/list-filters.component.ts
+++ src/app/shared/components/list-filters/list-filters.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { cleanText } from '../../../../utils/text';
-import { IPropertyLabel, IPropertyValue } from '../../../core/interfaces/common';
+import { IPropertyLabel } from '../../../core/interfaces/common';
 
 @Component({
   selector: 'ts-list-filters',
@@ -15,16 +15,19 @@ export class ListFiltersComponent implements OnInit {
   public form: FormGroup = new FormGroup({});
   @Input() dataToFilter: any[] = [];
   @Input() filterByProperties: IPropertyLabel[] = [];
+  @Input() sortByProperties: IPropertyLabel[] = [];
   @Output() search: EventEmitter<string> = new EventEmitter<string>();
   @Output() filterData: EventEmitter<any> = new EventEmitter<any>();
-  filterByValue: any = {};
+  filterByValues: any = {};
+  orderByValue = '';
 
   ngOnInit(): void {
       this.form = new FormGroup ({
-        search: new FormControl( '' ),
+        search: new FormControl( ''),
       });
       this.form.controls.search.valueChanges.subscribe(value => this.onSearch(value));
       this.filterData.emit(this.dataToFilter);
+      this.resetSortBy();
   }
 
   onSearch(value: string): void {
@@ -33,15 +36,38 @@ export class ListFiltersComponent implements OnInit {
     this.filterData.emit(dataFiltered);
   }
 
-  filterDataBy(property: string, value: string): void {
-    this.filterByValue[property] = value;
-    const filtered = this.dataToFilter.filter(item => item[property] === value);
+  applyFilters(): void {
+    const filtered = this.dataToFilter.filter(item => {
+      const filterResults = Object.keys(this.filterByValues)
+        .map(filterProperty => item[filterProperty] === this.filterByValues[filterProperty]).reduce((a, b) => a && b, true);
+      return filterResults;
+    });
     this.filterData.emit(filtered);
   }
 
+  filterDataBy(property: string, value: string): void {
+    this.filterByValues[property] = value;
+    this.applyFilters();
+  }
+
   resetFilterBy(property: string): void {
-    this.filterByValue[property] = null;
-    this.filterData.emit(this.dataToFilter);
+    delete this.filterByValues[property];
+    this.applyFilters();
+  }
+
+  sortDataBy(orderBy: IPropertyLabel): void {
+    const { label, property } = orderBy;
+    this.orderByValue = label;
+    const sortedData = this.dataToFilter.sort((a, b) => {
+      const x = a[property];
+      const y = b[property];
+      return x < y ? -1 : x > y ? 1 : 0;
+    });
+    this.filterData.emit(sortedData);
+  }
+
+  resetSortBy(): void {
+    this.sortDataBy(this.sortByProperties[0]);
   }
 
 }
