2bbfc46f824ec0d67dab9def43c73135089a0acc
Williams <wilfre_padilla@hotmail.com>
2021-05-13 12:09:53 -0400

    Refactor: replacing ng-bootstrap dropdown by primeng dropdown Feat: new dropdown component

---
+++ src/app/shared/components/dropdown/dropdown.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Dropdown } from 'primeng/dropdown';
+
+@Component({
+  selector: 'ts-dropdown',
+  templateUrl: './dropdown.component.html',
+  styleUrls: ['./dropdown.component.scss']
+})
+export class DropdownComponent implements OnInit {
+
+  constructor() { }
+
+  @Output() change: EventEmitter<{ value: any }> = new EventEmitter<{ value: any }>();
+  @Input() data: any[] = [];
+  @Input() placeholder = '';
+  @Input() label = '';
+  @Input() styleClass = '';
+  @Input() wrapperClass = '';
+  @Input() optionLabel = '';
+  @Input() showClear = false;
+  @Output() onClear: EventEmitter<Dropdown> = new EventEmitter<Dropdown>();
+
+  ngOnInit(): void {
+  }
+
+  onFocus($event: any): void {
+    $event.target.blur();
+  }
+}
--- src/app/shared/pipes/unique-values-array-elements.pipe.ts
+++ src/app/shared/pipes/unique-values-array-elements.pipe.ts
@@ -2,7 +2,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({name: 'uniqueValuesArrayElementsPipe'})
 export class UniqueValuesArrayElementsPipe implements PipeTransform {
-  transform(value: any[], property: string): string[] {
-    return [...new Set(value.map(item => item[property]))];
+  transform(value: any[], property: string, addedValues: string[] = []): string[] {
+    return [...addedValues, ...new Set(value.map(item => item[property]))];
   }
 }
