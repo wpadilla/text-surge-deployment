c02189e09c5d75c316de582119e780199d85a030
Williams <wilfre_padilla@hotmail.com>
2021-06-08 13:33:02 -0400

    Styles/Feat: styles for accordion

--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { ClientRoutingModule } from './client-routing.module';
 import { ClientListComponent } from './containers/client-list/client-list.component';
 import { SharedModule } from '../shared/shared.module';
+import { AccordionModule } from "primeng/accordion";
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { SharedModule } from '../shared/shared.module';
   ],
   imports: [
     ClientRoutingModule,
-    SharedModule
+    SharedModule,
+    AccordionModule
   ],
 })
 export class ClientModule {
