3d501c7f0fa308f4bcbe316624a3f56ac0273c69
Williams <wilfre_padilla@hotmail.com>
2021-07-06 18:05:53 -0400

    Feat: add presetsHeaders conditions in csvToArray function

--- src/utils/files.utils.ts
+++ src/utils/files.utils.ts
@@ -1,10 +1,12 @@
 export function csvToArray(str: string, delimiter = ',', presetsHeaders?: string[]): any[] {
   // slice from start of text to the first \n index
   // use split to create an array from string by delimiter
+  // presetHeaders received from params are more prioritized
   const headers = presetsHeaders || str.slice(0, str.indexOf('\n')).split(delimiter);
   // slice from \n index + 1 to the end of the text
   // use split to create an array of each csv value row
-  const rows = str.slice(str.indexOf('\n') + 1).split('\n');
+  // if presetsHeaders is set then we will split all text to divide the rows
+  const rows = presetsHeaders ? str.split('\n') : str.slice(str.indexOf('\n') + 1).split('\n');
 
   // Map the rows
   // split values from each row into an array
